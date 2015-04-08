6bd1d09d053d9c565fc3fdba4fd1a49e4adb9090
Thiago Garcia <thiagosgarcia@gmail.com>
2015-04-08 01:03:23 -0300

    FirstCommit

---
+++ Clickr/Clickr.Shared/Counter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clickr
+{
+    public class Counter
+    {
+        public Counter()
+        {
+            Count = 0;
+        }
+
+        private static Counter _counter = null;
+        public static Counter GetInstance()
+        {
+            return _counter = _counter ?? new Counter();
+        }
+        public int Count { get; set; }
+
+        public int Click()
+        {
+            return Count = Count > 99999 ? 0 : ++Count;
+        }
+
+        public int Reset()
+        {
+            return Count = 0;
+        }
+    }
+}
---
+++ Clickr/Clickr.WindowsPhone/MainPage.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Clickr
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: Prepare page for display here.
+
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+            HideWarning();
+        }
+        private void Grid_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            Count();
+        }
+
+        private void Reset_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            ShowWarning();
+        }
+
+        private void ResetLabel_DoubleTapped(object sender, DoubleTappedRoutedEventArgs e)
+        {
+            Reset();
+        }
+
+        private void ResetLabelWarn_DoubleTapped(object sender, DoubleTappedRoutedEventArgs e)
+        {
+            Reset();
+        }
+
+        private void ResetLabelWarn_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            Count();
+        }
+
+        private void HideWarning()
+        {
+            if (ResetLabelWarn.Visibility == Visibility.Visible)
+                ResetLabelWarn.Visibility = Visibility.Collapsed;
+        }
+
+        private void ShowWarning()
+        {
+            if (ResetLabelWarn.Visibility == Visibility.Collapsed)
+                ResetLabelWarn.Visibility = Visibility.Visible;
+        }
+
+        private void Count()
+        {
+            CounterLabel.Text = Counter.GetInstance().Click().ToString("00000");
+            HideWarning();
+        }
+
+        private void Reset()
+        {
+            CounterLabel.Text = Counter.GetInstance().Reset().ToString("00000");
+            HideWarning();
+        }
+    }
+}
